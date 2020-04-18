d627f2d36b20d03f04560f666d6a55805976114d
Vincent Rialland <vincent.rialland@gmail.com>
2020-04-18 18:20:20 +0200

    Fix pins setup

--- raspycar/controls.py
+++ raspycar/controls.py
@@ -4,10 +4,10 @@ from gpiozero import PWMOutputDevice
 
 
 class Pins(Enum):
-    FORWARD_LEFT = 26
-    FORWARD_RIGHT = 13
-    REVERSE_LEFT = 19
-    REVERSE_RIGHT = 6
+    FORWARD_LEFT = 18
+    FORWARD_RIGHT = 27
+    REVERSE_LEFT = 24
+    REVERSE_RIGHT = 10
 
 
 pwm_forward_left = PWMOutputDevice(Pins.FORWARD_LEFT.value, True, 0, 1000)
