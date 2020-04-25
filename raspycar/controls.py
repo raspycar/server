ce00b818961acd2b5464d614911b00cd73eaec33
Vincent Rialland <vincent.rialland@gmail.com>
2020-04-25 14:54:18 +0200

    Simplified controls

--- raspycar/controls.py
+++ raspycar/controls.py
@@ -13,10 +13,8 @@ class Pins(Enum):
     REVERSE_RIGHT = 10
 
 
-class Speed(Enum):
+class Throttle(Enum):
     NONE = 0.0
-    LOW = 0.2
-    HIGH = 0.8
     FULL = 1.0
 
 
@@ -29,70 +27,54 @@ try:
 
     def stop():
         """Stop all motion"""
-        pwm_forward_left.value = Speed.NONE.value
-        pwm_reverse_left.value = Speed.NONE.value
-        pwm_forward_right.value = Speed.NONE.value
-        pwm_reverse_right.value = Speed.NONE.value
+        pwm_forward_left.value = Throttle.NONE.value
+        pwm_reverse_left.value = Throttle.NONE.value
+        pwm_forward_right.value = Throttle.NONE.value
+        pwm_reverse_right.value = Throttle.NONE.value
 
     def forward():
         """Go forwards"""
-        pwm_forward_left.value = Speed.FULL.value
-        pwm_reverse_left.value = Speed.NONE.value
-        pwm_forward_right.value = Speed.FULL.value
-        pwm_reverse_right.value = Speed.NONE.value
+        pwm_forward_left.value = Throttle.FULL.value
+        pwm_reverse_left.value = Throttle.NONE.value
+        pwm_forward_right.value = Throttle.FULL.value
+        pwm_reverse_right.value = Throttle.NONE.value
 
     def reverse():
         """Go backwards"""
-        pwm_forward_left.value = Speed.NONE.value
-        pwm_reverse_left.value = Speed.FULL.value
-        pwm_forward_right.value = Speed.NONE.value
-        pwm_reverse_right.value = Speed.FULL.value
+        pwm_forward_left.value = Throttle.NONE.value
+        pwm_reverse_left.value = Throttle.FULL.value
+        pwm_forward_right.value = Throttle.NONE.value
+        pwm_reverse_right.value = Throttle.FULL.value
 
     def left():
         """Spin the car left"""
-        pwm_forward_left.value = Speed.NONE.value
-        pwm_reverse_left.value = Speed.NONE.value
-        pwm_forward_right.value = Speed.FULL.value
-        pwm_reverse_right.value = Speed.NONE.value
+        pwm_forward_left.value = Throttle.NONE.value
+        pwm_reverse_left.value = Throttle.NONE.value
+        pwm_forward_right.value = Throttle.FULL.value
+        pwm_reverse_right.value = Throttle.NONE.value
 
     def right():
         """Spin the car right"""
-        pwm_forward_left.value = Speed.FULL.value
-        pwm_reverse_left.value = Speed.NONE.value
-        pwm_forward_right.value = Speed.NONE.value
-        pwm_reverse_right.value = Speed.NONE.value
-
-    def forward_left():
-        """Turn left while driving forwards"""
-        pwm_forward_left.value = Speed.NONE.value
-        pwm_reverse_left.value = Speed.NONE.value
-        pwm_forward_right.value = Speed.HIGH.value
-        pwm_reverse_right.value = Speed.NONE.value
-
-    def forward_right():
-        """Turn right while driving forwards"""
-        pwm_forward_left.value = Speed.HIGH.value
-        pwm_reverse_left.value = Speed.NONE.value
-        pwm_forward_right.value = Speed.NONE.value
-        pwm_reverse_right.value = Speed.NONE.value
-
-    def reverse_left():
-        """Turn left while driving backwards"""
-        pwm_forward_left.value = Speed.NONE.value
-        pwm_reverse_left.value = Speed.NONE.value
-        pwm_forward_right.value = Speed.NONE.value
-        pwm_reverse_right.value = Speed.HIGH.value
-
-    def reverse_right():
-        """Turn right while driving backwards"""
-        pwm_forward_left.value = Speed.NONE.value
-        pwm_reverse_left.value = Speed.HIGH.value
-        pwm_forward_right.value = Speed.NONE.value
-        pwm_reverse_right.value = Speed.NONE.value
+        pwm_forward_left.value = Throttle.FULL.value
+        pwm_reverse_left.value = Throttle.NONE.value
+        pwm_forward_right.value = Throttle.NONE.value
+        pwm_reverse_right.value = Throttle.NONE.value
 
 
 except exc.BadPinFactory:
     logger.error("Unable to use pins, controls will not work!")
 
+    def stop():
+        logger.error('"stop" called')
 
-# TODO: https://www.bluetin.io/python/gpio-pwm-raspberry-pi-h-bridge-dc-motor-control/
+    def forward():
+        logger.error('"forward" called')
+
+    def reverse():
+        logger.error('"reverse" called')
+
+    def left():
+        logger.error('"left" called')
+
+    def right():
+        logger.error('"right" called')
